e39043ab320a9818e4628fe2741a50bcb463506b
Winston McGriff <winstonmcgriff27@gmail.com>
2021-02-18 19:24:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def find_min_in_nested_arrays(src)
   min_nested_arrays=[]
   row_index = 0
   while row_index < src.count do
-    min_nested_arrays << src[row_index].min?
+    min_nested_arrays << src[row_index].min
      row_index += 1
   end
   min_nested_arrays += 1
